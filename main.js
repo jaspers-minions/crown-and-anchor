d530aa56c4c86c85a7870c519cd23ab3ebfbc7a2
Marliana Lara <marliana.lara@gmail.com>
2017-01-09 21:39:36 -0500

    Add function to update selected symbols and the DOM.

--- main.js
+++ main.js
@@ -23,36 +23,53 @@ Calculate win/loss total and add to funds
   symbolsContainer.classList.add('symbolsContainer');
   document.body.insertBefore(symbolsContainer, document.body.lastElementChild);
 
+
   const fundsElement = document.createElement('p');
   fundsElement.innerHTML = funds + ' coins';
   document.body.insertBefore(fundsElement, document.body.firstElementChild);
 
+
   const symbolTable = {};
 
-
+  // create element for each symbol and add it to symbols container and symbols table
   symbols.forEach(symbol => {
-    let symbolElement = document.createElement('p');
+    const wrapper = document.createElement('div');
+    symbolsContainer.appendChild(wrapper);
+
+    const symbolElement = document.createElement('p');
     symbolElement.innerHTML = symbol;
-    symbolsContainer.appendChild(symbolElement);
+    wrapper.appendChild(symbolElement);
+
+    const betElement = document.createElement('input');
+    wrapper.appendChild(betElement);
+
+    const betButton = document.createElement('button');
+    betButton.textContent = 'Bet';
+    wrapper.appendChild(betButton);
+
     symbolTable[symbol] = symbolElement;
   });
-  debugger;
 
   function updateSelectedSymbols(event) {
     const symbol = event.target.textContent;
     const index = selectedSymbols.indexOf(symbol);
 
-    if (event.target !== this) {
-      if (index < 0) {
-        selectedSymbols.push(symbol);
-      } else {
-        selectedSymbols.splice(index, 1);
-      }
+    if (event.target === this) {
+      return;
     }
-    console.log(selectedSymbols);
+
+    if (index < 0 && selectedSymbols.length < 3) {
+      selectedSymbols.push(symbol);
+      symbolTable[symbol].classList.add('selected');
+    } else if (index < 0 && selectedSymbols.length === 3) {
+      return;
+    } else {
+      selectedSymbols.splice(index, 1);
+      symbolTable[symbol].classList.remove('selected');
+    }
   }
 
   symbolsContainer.addEventListener('click', updateSelectedSymbols);
-
+  
 
 })();
